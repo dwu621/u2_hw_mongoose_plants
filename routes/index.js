31fa30c2f5ded9f74b385ccaf3a1ea5127e4abf7
David Wu <dwu0508@gmail.com>
2022-03-23 00:38:19 -0500

    getAllPlants

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', (req, res) => res.send('This is landing page!'))
 
 router.post('/plants', controllers.createPlant)
 
+router.get('/plants', controllers.getAllPlants)
+
 module.exports = router;
 
 
